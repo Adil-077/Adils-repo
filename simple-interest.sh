9017754bae4fa21727120b1a1ecea84d3bec4032
Adil-077 <arhamadil48@gmail.com>
2025-11-29 19:45:41 +0530

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Simple Interest Calculator: (P * T * R) / 100
+echo "Enter Principal:"
+read p
+echo "Enter Time:"
+read t
+echo "Enter Rate:"
+read r
+si=$((p * t * r / 100))
+echo "Simple Interest = $si"
